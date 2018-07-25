88acf906c654e1d34f994796a738f7f37332e4f0
paceruva <40699661+paceruva@users.noreply.github.com>
2018-07-25 00:30:11 -0600

    Delete main.cpp

--- main.cpp
+++
@@ -1,9 +0,0 @@
-#include "Game.h"
-
-int main(int argc, char* argv[])
-{
-    /// Creates an instance of the class Game
-    Game game(argc, argv);
-    /// Starts the game
-    return game.run();
-}
